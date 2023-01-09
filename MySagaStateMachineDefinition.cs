4c7b518e2829456387455476c62851534f8c4568
Arnaud Thiry <arnaud.thiry@gentwo.com>
2023-01-09 16:31:06 +0100

    Add a derfinition.

---
+++ MySagaStateMachineDefinition.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using MassTransit;
+
+namespace MassTransitDemo
+{
+    public class MySagaStateMachineDefinition : SagaDefinition<MyState>
+    {
+        public MySagaStateMachineDefinition()
+        {
+            ConcurrentMessageLimit = 1;
+        }
+    }
+}
